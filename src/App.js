b041a89e7efa20f6bbcd97a3742a7449ec82c91c
FBB <apple@FBBdeMacBook-Pro.local>
2019-05-05 17:22:34 +0800

    添加模态框Modal组件

--- src/App.js
+++ src/App.js
@@ -1,33 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import './Toast/index.css'
 import Marquee from './Marquee';
 import RadioButton from './RadioButton';
 import CheckBox from './CheckBox';
 import MarqueeLine from './MarqueeLine';
-import Mask from './Mask';
+import Modal from './Modal';
 
-function App() {
-  return (
-    <div className="App">
-      <div className='item'>
-        <div className='label'>跑马灯：</div>
-        <Marquee />
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      visible: false
+    }
+  }
+
+  showModal = () => {
+    this.setState({
+      visible: true
+    })
+  }
+
+  handleCancel = () => {
+    console.log('Cancel')
+    this.setState({
+      visible: false
+    })
+  }
+
+  handleComfirm = () => {
+    console.log('Comfirm')
+    //数据处理...
+    this.setState({
+      visible: false
+    })
+  }
+
+  render() {
+    const { visible } = this.state
+    return (
+      <div className="App">
+        <div className='item'>
+          <div className='label'>跑马灯：</div>
+          <Marquee />
+        </div>
+        <div className='item'>
+          <div className='label'>RadioButton：</div>
+          <RadioButton options={[{ label: 'Vue', value: 'vue' }, { label: 'React', value: 'react' }, { label: 'Angular', value: 'angular' }, { label: 'Webpack', value: 'webpack' }]} />
+        </div>
+        <div className='item'>
+          <div className='label'>CheckBox：</div>
+          <CheckBox options={[{ label: 'Vue', value: 'vue' }, { label: 'React', value: 'react' }, { label: 'Angular', value: 'angular' }, { label: 'Webpack', value: 'webpack' }, { label: 'Nodejs', value: 'nodejs' }, { label: 'ES6', value: 'es6' }]} maxLength='3' />
+        </div>
+        <div className='item'>
+          <div className='label'>横向跑马灯：</div>
+          <MarqueeLine detail='陌生的人 空港的雨还在下 今夜的你要去向哪儿 陌生的城 大雨将你吞下 滴答滴答我们一起唱' />
+        </div>
+        <div className='item'>
+          <div className='label' onClick={this.showModal}>点我查看，模态框：</div>
+          <Modal visible={visible}
+            title='这是标题'
+            detail='这里是内容'
+            cancelText='取消'
+            comfirmText='确定'
+            onCancel={this.handleCancel}
+            onComfirm={this.handleComfirm}
+          />
+        </div>
       </div>
-      <div className='item'>
-        <div className='label'>RadioButton：</div>
-        <RadioButton options={[{ label: 'Vue', value: 'vue' }, { label: 'React', value: 'react' }, { label: 'Angular', value: 'angular' }, { label: 'Webpack', value: 'webpack' }]} />
-      </div>
-      <div className='item'>
-        <div className='label'>CheckBox：</div>
-        <CheckBox options={[{ label: 'Vue', value: 'vue' }, { label: 'React', value: 'react' }, { label: 'Angular', value: 'angular' }, { label: 'Webpack', value: 'webpack' }, { label: 'Nodejs', value: 'nodejs' }, { label: 'ES6', value: 'es6' }]} maxLength='3' />
-      </div>
-      <div className='item'>
-        <div className='label'>横向跑马灯：</div>
-        <MarqueeLine  detail='陌生的人 空港的雨还在下 今夜的你要去向哪儿 陌生的城 大雨将你吞下 滴答滴答我们一起唱'/>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/Modal/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Mask from '../Mask';
+import close from './close.png'
+import './index.css'
+
+export default function Modal(props) {
+  const { visible, title, detail, cancelText, comfirmText } = props
+
+  const handleCancel = () => {
+    props.onCancel()
+  }
+  
+  const handleConfirm = () => {
+    props.onComfirm()
+  }
+  
+  return (
+    <React.Fragment>
+      {visible &&
+        <div className='modal'>
+          <div className='modal--container'>
+            <div className='modal--header'>
+              <span>{title}</span>
+              <img src={close} alt='close' onClick={handleCancel} />
+            </div>
+            <div className='modal--body'>{detail}</div>
+            <div className='modal--footer'>
+              <div className='modal--button modal--button__cancel' onClick={handleCancel}>{cancelText}</div>
+              <div className='modal--button modal--button__comfirm' onClick={handleConfirm}>{comfirmText}</div>
+            </div>
+          </div>
+          <Mask />
+        </div>}
+    </React.Fragment>
+  )
+}
